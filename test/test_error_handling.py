9057f58ba2888ed5039f3bbba14edbc89b4a6f02
David Meijer <david-meijer@live.nl>
2022-07-16 15:06:03 +0200

    ADD: added two more unit tests for error handling

--- test/test_error_handling.py
+++ test/test_error_handling.py
@@ -3,7 +3,7 @@ from cinnamol import error_handler, Fail, Success
 
 
 class TestErrorHandling(TestCase):
-    def test_assert_fail_simple_function(self):
+    def test_assert_fail_simple_function_with_one_argument(self):
         """
         Check if failing a simple function results in Fail object result.
         """
@@ -12,13 +12,32 @@ class TestErrorHandling(TestCase):
         result = func(None)
         self.assertTrue(isinstance(result, Fail))
 
-    def test_assert_success_simple_function(self):
+    def test_assert_fail_simple_function_with_two_arguments(self):
+        """
+        Check if failing a simple function results in Fail object result.
+        """
+        @error_handler
+        def func(x, y): return x / y
+        result = func(None, 3)
+        self.assertTrue(isinstance(result, Fail))
+
+    def test_assert_success_simple_function_with_one_argument(self):
         """
         Check if succeeding a simple function results in Success object result.
         """
         @error_handler
         def func(x): return x / 2
         result = func(2)
+        self.assertTrue(isinstance(result, Success))
+
+
+    def test_assert_success_simple_function_with_two_arguments(self):
+        """
+        Check if failing a simple function results in Fail object result.
+        """
+        @error_handler
+        def func(x, y): return x / y
+        result = func(6, 3)
         self.assertTrue(isinstance(result, Success))
 
 
