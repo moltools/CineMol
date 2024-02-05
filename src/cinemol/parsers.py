bab558a3ec3bcc4174e773b0a7240e7b190750d5
David Meijer <david-meijer@live.nl>
2024-02-05 13:57:57 +0100

    BUG: update molfile parser to accommodate different atom and bond line lengths

--- src/cinemol/parsers.py
+++ src/cinemol/parsers.py
@@ -14,25 +14,29 @@ def parse_sdf(src: str, include_hs: bool = True) -> ty.Tuple[ty.List[Atom], ty.L
     atoms, bonds = [], []
 
     lines = src.split("$$$$\n")[0].split("\n")
-    
+
+    counts_line = lines[3]
+    atom_count_str, bond_count_str, *_ = counts_line.strip().split()
+    atom_count, bond_count = int(atom_count_str), int(bond_count_str)
+
+    atom_lines = lines[4:4 + atom_count]
+    bond_lines = lines[4 + atom_count:4 + atom_count + bond_count]
+
     atom_index = 0
-    for line in lines[4:]:
-                
-        if line.strip() == "M  END":
-            break
 
-        parts = line.split()
-
-        # Parse atom line.
-        if len(parts) == 16:
-            atom_index += 1
-            x, y, z, atom_symbol = parts[0], parts[1], parts[2], parts[3]
-            x, y, z = float(x), float(y), float(z)
-            atoms.append(Atom(atom_index, atom_symbol, (x, y, z)))
+    # Parse atom line.
+    for atom_line in atom_lines:
+        atom_index += 1
+        parts = atom_line.strip().split()
+        x, y, z, atom_symbol = parts[0], parts[1], parts[2], parts[3]
+        x, y, z = float(x), float(y), float(z)
+        atoms.append(Atom(atom_index, atom_symbol, (x, y, z)))
         
-        elif len(parts) == 7:
-            start_index, stop_index, bond_order = parts[0], parts[1], parts[2]
-            bonds.append(Bond(int(start_index), int(stop_index), int(bond_order)))
+    # Parse bond line.
+    for bond_line in bond_lines:
+        parts = bond_line.strip().split()
+        start_index, stop_index, bond_order = parts[0], parts[1], parts[2]
+        bonds.append(Bond(int(start_index), int(stop_index), int(bond_order)))
 
     atom_map = {atom.index: atom for atom in atoms}
     if not include_hs:
