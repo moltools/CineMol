3784dc8d984ff3fb4ea18400e5f61021dbe36230
David Meijer <david-meijer@live.nl>
2024-04-30 14:52:22 +0200

    REF: changing access to modules

--- src/cinemol/__init__.py
+++ src/cinemol/__init__.py
@@ -2,4 +2,10 @@
 
 """CineMol is a tool for drawing small molecule configurations directly to SVG."""
 
-from .api import *  # noqa
+from .chemistry import *
+from .fitting import *
+from .geometry import *
+from .model import *
+from .parsers import *
+from .style import *
+from .svg import *
