b8212b6ea4c7cf1bd4d21bc649e8ae9c823536a8
David Meijer <david-meijer@live.nl>
2024-05-06 14:23:35 +0200

    WIP: adding tests for model module

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,24 +4,15 @@
 
 import unittest
 
-from cinemol.geometry import CylinderCapType, Cylinder, Line3D, Sphere, Point3D
-from cinemol.model import (
-    ModelCylinder,
-    ModelSphere,
-    ModelWire,
-    Scene,
-    calculate_intersecting_nodes,
-    create_fill,
-    get_node_polygon_vertices,
-    prepare_nodes_for_intersecting,
-)
+from cinemol.geometry import Cylinder, CylinderCapType, Line3D, Point3D, Sphere
+from cinemol.model import ModelCylinder, ModelSphere, ModelWire, Scene
 from cinemol.style import Cartoon, Color
 
 
 class TestModelSphere(unittest.TestCase):
     """Test the ModelSphere class."""
 
-    def test_init(self):
+    def test_creation_model_sphere(self):
         """Test ModelSphere creation."""
         sphere = Sphere(Point3D(1, 2, 3), 1.0)
         depiction = Cartoon(Color(0, 255, 0))
@@ -33,14 +24,9 @@ class TestModelSphere(unittest.TestCase):
 class TestModelCylinder(unittest.TestCase):
     """Test the ModelCylinder class."""
 
-    def test_init(self):
+    def test_creation_model_cylinder(self):
         """Test ModelCylinder creation."""
-        cylinder = Cylinder(
-            Point3D(0, 0, 0),
-            Point3D(0, 0, 1),
-            1.0,
-            CylinderCapType.NO_CAP
-        )
+        cylinder = Cylinder(Point3D(0, 0, 0), Point3D(0, 0, 1), 1.0, CylinderCapType.NO_CAP)
         depiction = Cartoon(Color(0, 255, 0))
         model = ModelCylinder(cylinder, depiction)
         self.assertEqual(model.geometry, cylinder)
@@ -50,7 +36,7 @@ class TestModelCylinder(unittest.TestCase):
 class TestModelWire(unittest.TestCase):
     """Test the ModelWire class."""
 
-    def test_init(self):
+    def test_creation_model_wire(self):
         """Test ModelWire creation."""
         line = Line3D(Point3D(0, 0, 0), Point3D(0, 0, 1))
         color = Color(0, 255, 0)
@@ -61,3 +47,20 @@ class TestModelWire(unittest.TestCase):
         self.assertEqual(model.color, color)
         self.assertEqual(model.width, width)
         self.assertEqual(model.opacity, opacity)
+
+
+class TestScene(unittest.TestCase):
+    """Test the Scene class."""
+
+    def test_creation_empty_scene(self):
+        """Test creation of Scene without supplying nodes."""
+        scene = Scene()
+        self.assertEqual(scene.nodes, [])
+
+    def test_createion_scene_with_single_node(self):
+        """Test creation of Scene with a single node."""
+        sphere = Sphere(Point3D(1, 2, 3), 1.0)
+        depiction = Cartoon(Color(0, 255, 0))
+        model = ModelSphere(sphere, depiction)
+        scene = Scene([model])
+        self.assertEqual(scene.nodes, [model])
